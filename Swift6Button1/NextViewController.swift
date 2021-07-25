74bb76d45e3c67a1d17b0fc516f7c909643be101
Shouta Tarukawa <shouta859@gmail.com>
2021-07-25 12:11:03 +0900

    app push

---
+++ Swift6Button1/NextViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NextViewController.swift
+//  Swift6Button1
+//
+//  Created by 樽川翔太 on 2021/07/24.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func change(_ sender: Any) {
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift6Button1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Swift6Button1
+//
+//  Created by 樽川翔太 on 2021/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //画面がロードされたとき
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
